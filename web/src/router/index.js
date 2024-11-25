aaa950c4a5a696a43ec53e82b17ec51f53d75c35
fudu <2221773420@qq.com>
2024-11-25 22:56:49 +0800

    创建导航栏

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView'
+import NotFoundIndexView from '../views/error/NotFoundIndexView'
 
 const routes = [
-  
+  {
+    path: "/",
+    redirect: "/pk/",
+    name: "home",
+    component: PkIndexView,
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFoundIndexView,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/",
+    component: NotFoundIndexView,
+  }
 ]
 
 const router = createRouter({
